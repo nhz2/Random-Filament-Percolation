1436b8d96e25b1f6f8cb71026503fc17c2021eb0
nhz2 <nhz2@cornell.edu>
2022-05-03 01:39:22 -0400

    final plots

--- angle-plots.jl
+++ angle-plots.jl
@@ -1,4 +1,4 @@
-using GLMakie
+using CairoMakie
 import Makie
 using LinearAlgebra
 using DataFrames
@@ -6,20 +6,29 @@ import CSV
 
 df = DataFrame(CSV.File("N100000 L10 R106 dc1.csv"))
 #df = DataFrame(CSV.File("N:10000,L:10.0,R:49.27619103464619,dc:1.0.csv"))
+N = 100000
 
-fig = Figure()
+begin
+fig = Figure( fontsize = 20)
 ax1 = Axis(fig[1,1])
 ax2 = Axis(fig[2,1])
-s10 = lines!(ax1,df.links,df.max_compsize_rand)
-s11 = lines!(ax1,df.links,df.max_compsize_para)
-s12 = lines!(ax1,df.links,df.max_compsize_perp)
-s20 = lines!(ax2,df.links,df.global_cluster_coeff_rand)
-s21 = lines!(ax2,df.links,df.global_cluster_coeff_para)
-s22 = lines!(ax2,df.links,df.global_cluster_coeff_perp)
+s10 = lines!(ax1,df.links,df.max_compsize_rand./N; label="Random")
+s11 = lines!(ax1,df.links,df.max_compsize_para./N; label="∥ first")
+s12 = lines!(ax1,df.links,df.max_compsize_perp./N; label="⟂ first")
+s20 = lines!(ax2,df.links,df.global_cluster_coeff_rand; label="Random")
+s21 = lines!(ax2,df.links,df.global_cluster_coeff_para; label="∥ first")
+s22 = lines!(ax2,df.links,df.global_cluster_coeff_perp; label="⟂ first")
 xlims!.([ax1,ax2], 0, nothing)
+ax2.xlabel = "Links added"
+ylims!(ax1, 0.0, 1)
+ax1.ylabel = "Max component size / N"
 ylims!(ax2, 0.0, 0.3)
-# ax1.yscale = log10
-# ax2.yscale = log10
-# ax1.xscale = identity
-# ax2.xscale = identity
+ax2.ylabel = "Global clustering coeff"
+ax1.yticks = LinearTicks(4)
+ax2.yticks = LinearTicks(3)
+axislegend(ax1; position=:rb)
+#Legend(fig[1:2, 2],[s11,s10,s12],["∥ first","Random","⟂ first"])
 fig
+end
+
+save("angle-plots.svg",fig)
--- phase-plots.jl
+++ phase-plots.jl
@@ -1,10 +1,23 @@
-using GLMakie
+using CairoMakie
 import Makie
 using LinearAlgebra
 using DataFrames
 import CSV
 
 df = DataFrame(CSV.File("phase-data.csv"))
+
+fig = Figure( fontsize = 25)
+ax1, s1 = scatter(fig[1,1],df.Ls,df.dc_crits;
+    markersize=3,
+)
+xlims!(ax1, 0, 4)
+ax1.yscale = identity
+ax1.xscale = identity
+ax1.xlabel = "L"
+ax1.ylabel = "Critical cutoff distance"
+ax1.title = "Critical cutoff distance vs L at N = 10⁶ and V = 4N"
+fig
+save("Critical-points-raw.svg",fig)
 
 # norm so dc_crits = 1.0
 
@@ -13,12 +26,25 @@ segment_conc_normed = @.(df.N/Vnormed)
 segment_length_normed = @.(df.Ls/df.dc_crits)
 monomer_conc_normed = @.(segment_conc_normed * segment_length_normed)
 
-fig = Figure()
-ax1, s1 = scatter(fig[1,1],segment_length_normed,(segment_conc_normed))
-ax2, s2 = scatter(fig[2,1],segment_length_normed,(monomer_conc_normed))
+begin
+fig = Figure( fontsize = 16)
+ax1, s1 = scatter(fig[1,1],segment_length_normed,segment_conc_normed;
+    markersize=3,
+)
+ax2, s2 = scatter(fig[2,1],segment_length_normed,monomer_conc_normed;
+    markersize=3,
+)
 xlims!.([ax1,ax2], 0.0, 10)
+ax2.xlabel = "Aspect ratio: L/dc"
 ax1.yscale = log10
 ax2.yscale = log10
 ax1.xscale = identity
 ax2.xscale = identity
+ax1.ylabel = "Segment conc: N / (V/dc³)"
+ax2.ylabel = "Monomer conc: N*(L/dc) / (V/dc³)"
+ax1.xticks = LinearTicks(10)
+ax2.xticks = LinearTicks(10)
 fig
+end
+
+save("Critical-points-normed.svg",fig)
